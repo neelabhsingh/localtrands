36b9fe801cc83b5f549de5eb1f92550fe5f53393
Neelabh Singh <neelabhsingh1986@gmail.com>
2024-06-14 14:45:32 +0530

    Add All services for localtrands

---
+++ marketplace-service/src/test/java/com/localtrends/marketplaceservice/MarketplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localtrends.marketplaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
